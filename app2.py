074c9930362982ad6e211c6290e4eae1b3bfbeac
Rajkap <34119577+Rajkap@users.noreply.github.com>
2021-09-24 18:32:04 +0630

    Update app2.py

--- app2.py
+++ app2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import os
 
-image_library = {'Obama':'obama','Daw Su':'daw_su','Jackie':'jackie_chan','Messi''messi'}
+image_library = {'Obama':'obama','Daw Su':'daw_su','Jackie':'jackie_chan','Messi':'messi'}
 
 img_dirs = pd.DataFrame()
 #img_dirs['dir'] = ['obama','daw_su','jackie_chan','messi']
